669f1819e77da89695b64d344337fd8c496c8d8f
Loan Marchand <loan.marchand7@gmail.com>
2024-09-23 15:19:19 +0200

    install prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,32 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from "./pages/HomePage";
-import About from "./pages/AboutPage";
-import Contact from "./pages/ContactPage";
-import NotFoundPage from "./pages/NotFoundPage";
-import Projects from "./pages/ProjectsPage";
+import Home from './pages/HomePage';
+import About from './pages/AboutPage';
+import Contact from './pages/ContactPage';
+import NotFoundPage from './pages/NotFoundPage';
+import Projects from './pages/ProjectsPage';
 import Header from './containers/Header';
-import Footer from "./containers/Footer.tsx";
+import Footer from './containers/Footer.tsx';
 import { Analytics } from '@vercel/analytics/react';
 
 
 function App() {
     return (
+      <div className="flex flex-col min-h-screen">
         <BrowserRouter>
-            <Header /> {/* Le header est en dehors des routes pour qu'il soit visible sur toutes les pages */}
+          <Header /> {/* Le header est en dehors des routes pour qu'il soit visible sur toutes les pages */}
+          <main className="flex-grow">
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/About" element={<About />} />
-                <Route path="/Contact" element={<Contact />} />
-                <Route path="/Projects" element={<Projects />} />
-                <Route path="*" element={<NotFoundPage />} />
+              <Route path="/" element={<Home />} />
+              <Route path="/About" element={<About />} />
+              <Route path="/Contact" element={<Contact />} />
+              <Route path="/Projects" element={<Projects />} />
+              <Route path="*" element={<NotFoundPage />} />
             </Routes>
-            <Footer />
-            <Analytics />
+          </main>
+          <Footer />
+          <Analytics />
         </BrowserRouter>
-
+      </div>
     );
 }
 
--- src/components/GithubProjects.tsx
+++ src/components/GithubProjects.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 // Définition de l'interface pour un projet GitHub
 interface GithubRepo {
@@ -26,25 +26,20 @@ const GithubProjects: React.FC = () => {
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             {repos.map((repo) => (
-                <div
+              <a
                     key={repo.id}
-                    className="bg-gradient-to-r from-blue-500 to-purple-500 rounded-lg p-1"
-                >
-                    <div className="bg-white rounded-lg shadow-lg p-6 hover:shadow-2xl transform hover:scale-105 transition-all duration-300">
-                        <h3 className="text-2xl font-bold mb-2">{repo.name}</h3>
-                        <p className="text-gray-600 mb-4">
+                    href={repo.html_url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="block bg-gradient-to-r from-blue-500 to-purple-500 rounded-lg p-1 transition-all duration-300 hover:scale-105"
+              >
+                <div className="bg-white rounded-lg shadow-lg p-6 hover:shadow-2xl">
+                  <h3 className="text-2xl font-bold mb-2">{repo.name}</h3>
+                  {/*<p className="text-gray-600 mb-4">
                             {repo.description || "Pas de description disponible."}
-                        </p>
-                        <a
-                            href={repo.html_url}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="inline-block text-white bg-blue-500 hover:bg-blue-700 py-2 px-4 rounded-lg"
-                        >
-                            Voir sur GitHub
-                        </a>
+                        </p>*/}
                     </div>
-                </div>
+              </a>
             ))}
         </div>
     );
